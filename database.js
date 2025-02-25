8325383546e62eaa4a0f794ce031a8406674ee2e
niknak1379 <nikanostovan@gmail.com>
2025-02-25 14:17:33 -0800

    commit

--- database.js
+++ database.js
@@ -1,33 +1,50 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const DB = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '78M56Soo!',
-    database: 'Projects'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
 
-let hi = await DB.query("SELECT * FROM Projects;")
-console.log(hi)
 
 
 //Does not support no tags at the moment, could just do an all tag and cheese it
+//Doest return duplicates atm, should change that.
+//could make the tags not show on the list page of the projects but on the dedicated
+//page of each project instead.
 export async function getProjects(searchQueryStr, statusStr, tagsArray) {
     if (tagsArray.length == 0) {
-        console.log('hi')
+        //console.log('hi')
     }
    
-    let hi = await DB.query(`
+    let queryWithDuplicates = await DB.query(`
         SELECT * FROM Projects 
         JOIN ProjectTags ON Projects.name = ProjectTags.name
         WHERE ProjectTags.tag IN (?)
         AND Projects.name LIKE ?
         AND Projects.status LIKE ?
         `, [tagsArray, `%${searchQueryStr}%`, `%${statusStr}%`])
-    return hi[0]
+
+    //handle duplicates
+    let set = new Set()
+    let returnList = []
+    //console.log(queryWithDuplicates[0][0]);
+    for (let i = 0; i < queryWithDuplicates[0].length; i++){
+        //console.log(queryWithDuplicates[0][i].name)
+        if (set.has(queryWithDuplicates[0][i].name)){
+            continue
+        }
+        set.add(queryWithDuplicates[0][i].name)
+        returnList.push(queryWithDuplicates[0][i])
+    }
+    
+    return returnList
 }
 
-getProjects("nik", "", ["PYTHON"])
+//getProjects("nik", "", ["PYTHON", "WEBDEV"])
 export async function insertProject(projectObject, tagsObject){
     let name = projectObject.name
     let description = projectObject.description
