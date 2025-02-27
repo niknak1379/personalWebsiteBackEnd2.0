5f95e13d86c537cd9e17a95d70c0e502bcdfbe99
niknak1379 <nikanostovan@gmail.com>
2025-02-26 16:22:54 -0800

    completed insert function

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import {getProjects, insertProject} from './database.js'
 const app = express()
-app.get('/:id/:description/:tags', async (req, res) => {
+app.use(express.json())
+
+app.get('/:name/:description/:tags', async (req, res) => {
     console.log(req.params)
     //send in space for an empty paramter
+    if (req.params.name == ' '){
+        req.params.name = ''
+    }
     if (req.params.description == ' '){
         req.params.description = ''
     }
@@ -15,10 +20,37 @@ app.get('/:id/:description/:tags', async (req, res) => {
         req.params.tags = req.params.tags.split('-')
     }
     console.log(req.params)
-    const notes = await getProjects(`${req.params.id}`, `${req.params.description}`, req.params.tags)
-    console.log(notes)
-    res.send(notes)
+    const projects = await getProjects(`${req.params.name}`, `${req.params.description}`, req.params.tags)
+    console.log(projects)
+    res.send(projects)
+})
+/*
+post data expected format
+   { 
+   "project": {
+      "name": "nik",
+      "description": "longs d fs sdj ds ",
+      "status": "Complete",
+      "pictureURL": "pic url",
+      "githubURL": "git url",
+      "deploymentURL": "dep url",
+      "tag": "ALL"
+    }
+    ,
+    "tags": ["PYTHON", "ALL"]
+    }
+*/
+app.post('/',  (req, res) => {
+    console.log(req.body.project)
+    console.log(req.body.tags)
+    insertProject(req.body.project, req.body.tags)
+    res.send(req.body)
 })
 app.listen(8080, () => {
     console.log('Server running on 8080')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("DataBase Crashed :(")
 })
