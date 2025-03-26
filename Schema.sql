a8eb800c3f79a548b57709f41654dd1f0eb02ed5
niknak1379 <nikanostovan@gmail.com>
2025-03-26 14:20:37 -0700

    added detailed project description to the schema

--- Schema.sql
+++ Schema.sql
@@ -11,18 +11,22 @@ CREATE TABLE Tags (
 );
 
 CREATE TABLE Projects (
-    name varchar(255) NOT NULL,
-    description TEXT NOT NULL,
+    name varchar(28) NOT NULL,
+    description varchar(135) NOT NULL,
+    longDescription Text NOT NULL,
     status varchar(255) NOT NULL,
-    pictureURL TEXT,
-    githubURL TEXT,
-    deploymentURL TEXT,
+    pictureURL varchar(255),
+    githubURL varchar(255),
+    deploymentURL varchar(255),
+    carouselImage_1 varchar(255),
+    carouselImage_2 varchar(255),
+    carouselImage_3 varchar(255),
     PRIMARY KEY (name),
     FOREIGN KEY (status) REFERENCES Status(status)
 );
 
 CREATE TABLE ProjectTags (
-    name varchar(255) NOT NULL,
+    name varchar(28) NOT NULL,
     tag varchar(255) NOT NULL,
     FOREIGN KEY (name) REFERENCES Projects(name),
     FOREIGN KEY (tag) REFERENCES Tags(tag)
@@ -64,10 +68,46 @@ END $$
 DELIMITER ;
 
 INSERT INTO Projects VALUES
-("Nikan's Personal Website V1", "My very own personal website, Showcasing my Projects and interests. Deployed on Vercel", 'Complete', 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/personalSS.avif', 'https://github.com/niknak1379/personalWebsite', 'https://nikanostovan.dev/'),
-("Persian American Student Association's Website", "Website Showcasing the student organization, its members, events and projects", 'Complete', 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/PASA-SS.avif', 'https://github.com/niknak1379/pasa-website', 'https://pasa.org/'),
-("Personal Home Lab", "Features currently include: PiHole for network wide adblock, Home-Assistant for IoT management", 'Complete', 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/PASA-SS.avif', 'https://github.com/niknak1379/pasa-website', 'https://pasa.org/'),
-("Nikan's Personal Website V2", "My very own personal website, Showcasing my Projects and interests. Deployed on AWS, using more sophisticated networking approaches.", 'In Progress', 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/personalSS.avif', 'https://github.com/niknak1379/personalWebsite2.0', 'https://nikanostovan.dev/');
+("Nikan's Personal Website V1", 
+'My very own personal website, Showcasing my Projects and interests. Deployed on Vercel, using mostly vanilla JS and some React', 
+"LONG DESC GOES HERE",
+'Complete', 
+'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/personalSS.avif', 
+'https://github.com/niknak1379/personalWebsite', 
+'https://nikanostovan.dev/',
+'CarouselURL 1 goes here',
+'CarouselURL 2 goes here',
+'CarouselURL 3 goes here'),
+("PASA club's Website", 
+'Website Showcasing the student organization, its members, events and projects, and resources for those interested in the club', 
+"LONG DESC GOES HERE",
+'Complete', 
+'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/PASA-SS.avif', 
+'https://github.com/niknak1379/pasa-website', 
+'https://pasa.org/',
+'CarouselURL 1 goes here',
+'CarouselURL 2 goes here',
+'CarouselURL 3 goes here'),
+("Personal Home Lab", 
+'Features currently include: PiHole for network wide adblock, Home-Assistant for IoT management, will add NAS and other functinoaliteis', 
+"LONG DESC GOES HERE",
+'Complete', 
+'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/PASA-SS.avif', 
+'https://github.com/niknak1379/pasa-website', 
+'https://pasaatucsd.org/',
+'CarouselURL 1 goes here',
+'CarouselURL 2 goes here',
+'CarouselURL 3 goes here'),
+("Nikan's Personal Website V2", 
+'My very own personal website, Showcasing my Projects and interests. Deployed on AWS, using more sophisticated networking approaches.', 
+"LONG DESC GOES HERE",
+'In Progress', 
+'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/personalSS.avif', 
+'https://github.com/niknak1379/personalWebsite2.0', 
+'https://nikanostovan.dev/',
+'CarouselURL 1 goes here',
+'CarouselURL 2 goes here',
+'CarouselURL 3 goes here');
 
 
 INSERT INTO ProjectTags(name, tag) VALUES 
@@ -83,9 +123,9 @@ INSERT INTO ProjectTags(name, tag) VALUES
 ("Nikan's Personal Website V2", "Networking");
 
 INSERT INTO ProjectTags(name, tag) VALUES 
-("Persian American Student Association's Website", "React"),
-("Persian American Student Association's Website", "Tailwind"),
-("Persian American Student Association's Website", "FrontEnd");
+("PASA club's Website", "React"),
+("PASA club's Website", "Tailwind"),
+("PASA club's Website", "FrontEnd");
 
 INSERT INTO ProjectTags(name, tag) VALUES 
 ("Personal Home Lab", "Raspberry-Pi"),
