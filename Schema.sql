174fce64ab7b716ebbf5c4eb085edb5e77e51cb0
niknak1379 <nikanostovan@gmail.com>
2025-03-28 13:33:25 -0700

    updated initial DB  values

--- Schema.sql
+++ Schema.sql
@@ -71,7 +71,9 @@ DELIMITER ;
 INSERT INTO Projects VALUES
 ("Nikan's Personal Website V1", 
 'My very own personal website, Showcasing my Projects and interests. Deployed on Vercel, using mostly vanilla JS and some React', 
-"LONG DESC GOES HERE",
+"My very own personal website, Showcasing my Projects and interests. Deployed on Vercel, using mostly vanilla JS and some React,
+Mostly used Vanilla JS in order to learn JS fundamentals and JS event loop and basic API implementations, and service workers. Only used some react on
+the main page to familiarize myself with the syntax and how it works. Backend for emailing and contact information processing is written with Express.js and hosted on Vercell",
 'Complete', 
 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/personalSS.avif', 
 'https://github.com/niknak1379/personalWebsite', 
@@ -81,8 +83,10 @@ INSERT INTO Projects VALUES
 'CarouselURL 2 goes here',
 'CarouselURL 3 goes here'),
 ("PASA club's Website", 
-'Website Showcasing the student organization, its members, events and projects, and resources for those interested in the club', 
-"LONG DESC GOES HERE",
+'Website Showcasing the student organization, its members, events and projects, and resources for those interested in the club.', 
+"Website Showcasing the student organization, its members, events and projects, and resources for those interested in the club.
+It is written in react and mostly uses Tailwind for styling. Does not have any app functionality or dedicated APIs as of yet, 
+will most likely add a custom mailing list app to it for the club to use. Mostly focused on desiging and CSS on thisi project",
 'Complete', 
 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/PASA-SS.avif', 
 'https://github.com/niknak1379/pasa-website', 
@@ -93,7 +97,9 @@ INSERT INTO Projects VALUES
 'CarouselURL 3 goes here'),
 ("Personal Home Lab", 
 'Features currently include: PiHole for network wide adblock, Home-Assistant for IoT management, will add NAS and other functinoaliteis', 
-"LONG DESC GOES HERE",
+"My Personal Homelab currently running on 2 different Raspberry-pis. One running Home-Assistant for IoT management and the other PiHole for network wide adblock. 
+will mostlikely add NAS and other functinoaliteis if I need more space for storing images and photography related files.
+",
 'Complete', 
 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/PASA-SS.avif', 
 'https://github.com/niknak1379/pasa-website', 
@@ -104,7 +110,9 @@ INSERT INTO Projects VALUES
 'CarouselURL 3 goes here'),
 ("Nikan's Personal Website V2", 
 'My very own personal website, Showcasing my Projects and interests. Deployed on AWS, using more sophisticated networking approaches.', 
-"LONG DESC GOES HERE",
+"My very own personal website, Showcasing my Projects and interests. Backend used for storing the database containing the projects and 
+contacting and email functinality is written in Express.js and hosted on Docker containers in AWS. Migrated the majority of the vanilla HTML and JS
+to react to further familiarize myself with the framework and how it interacts with server side APIs. The app is client side rendered completely.",
 'In Progress', 
 'https://www.nikanostovan.dev/Assets/App/Cards/ProjectPics/personalSS.avif', 
 'https://github.com/niknak1379/personalWebsite2.0', 
